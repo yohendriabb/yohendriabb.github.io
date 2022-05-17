5a5ad4bcf70eedd73c98ea76275d5aba0074f067
Yohendri <elverduvo@gmail.com>
2022-05-17 01:40:34 -0400

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,60 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <div id="main-container">
+    <header>
+      <nav>
+        <div class="mobile hide">
+          <div class="header">
+            <button id="bmenu"><span class="material-symbols-outlined">menu</span></button>
+            <router-link to="/about"><a href="#" >Microsofst</a></router-link>
+          </div>
+          <div class="links">
+            <a href="#">Game Pass</a>
+            <a href="#">Games</a>
+            <a href="#">Devices</a>
+            <a href="#">Cloud Gaming</a>
+            <a href="#">Comunnity</a>
+            <a href="#">Support</a>
+            <a href="#">My Xbox</a>
+            <a href="#">All Microsoft</a>
+            <a href="#">Search</a>
+          </div>
+          <div>
+            <router-link to="/home"><a href="#" ><span class="material-symbols-outlined">
+              home
+            </span></a></router-link>
+            <router-link class="none" to="/about"><a href="#" >Abouta</a></router-link>
+            <router-link to="/contact"><a href="#" >Contacts></a></router-link>
+          </div>
+        </div>
+        <ul>
+          <li><a href="#">YouDev</a></li>
+          <router-link class="none link" to="/"><li><a  class="link">Home</a></li></router-link>
+          <router-link class="none" to="/about"><li><a  class="link">About</a></li></router-link>
+          <li class="more">
+            <a href="#" class="link hide" id="bmore">More</a>
+            <div class="menu hide">
+              <a href="#" >Game Pass</a>
+              <a href="#" >Games</a>
+              <a href="#" >Devices</a>
+              <a href="#" >Support</a>
+              <a href="#" >My Xbox</a>
+            </div>
+          </li>
+        </ul>
+        <ul class="hide">
+         <li><a href="#" class="link">Search</a></li>
+         <li><a href="#" class="link">Cart</a></li>
+         <li><a href="#" class="link">Sing In</a></li>
+       </ul>
+     </nav>
+   </header>
+  
+ </div>
+
+<router-view/>
+
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
